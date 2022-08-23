ffb9642ffa15b914764b53a619e257f39f584d0e
ris <rismarizki5@gmail.com>
2022-08-23 09:39:37 +0700

    ris

---
+++ tambah.php
@@ -0,0 +1,14 @@
+<selection class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-0">
+            <div class="col-sm-12">
+                <h1 align="center">Form Tambah Pendaftar Kerja Prakter</h1>
+            </div>
+        </div>
+    </div>
+</selection>
+<!--Skrip Simpan Data Baru-->
+<?php
+if(isset($_POST['Id'])) {
+    
+}
